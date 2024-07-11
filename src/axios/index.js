2a999baa8464ade35c69699338b7867ac5796d8e
khoa <buidangkhoa252001@gmail.com>
2024-07-11 08:28:32 +0700

    change cors

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,14 +3,7 @@ const BASE_URL = import.meta.env.VITE_URI_SERVER
 console.log(BASE_URL)
 /* const BASE_URL = "http://localhost:8000"; */
 const instance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  },
+  baseURL: BASE_URL
 });
 
 export default instance;
